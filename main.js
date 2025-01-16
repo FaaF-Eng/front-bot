33dffc7a0942a8829bc60c025538a9ed62c8e0a3
Fábio Teixeira <fabio.teixeira18@hotmail.com>
2025-01-16 16:49:13 +0000

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function enviarMensagem(){ //Função para enviar a mensagem, por enquanto vai a
 document.addEventListener('DOMContentLoaded', () => {
     let contador = 0; // Inicializa um contador para gerar IDs únicos para as tarefas
 
-    // Define uma ação ao clicar no botão "Adicionar Tarefa"
+    // Define uma ação ao clicar no botão "Adicionar Tarefa" 
     document.getElementById('button').onclick = function() {
 
         // Captura o valor inserido no campo de texto
@@ -32,45 +32,63 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 });
 
+// [TROCANDO COR DO SITE]
+
+'use strict'
+
+const mudando= document.querySelector('.botão');
+
+mudando.addEventListener('click',function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className=='light-theme') {
+        this.textContent = "Dark";
+    }
+    else{
+        this.textContent = "Light";
+    }
+    console.log('currente class name:' + className);
+});
 
 
-// [CRUD BÁSICO]
-const miniRede = {
-    usuarios:[{username: 'fabio'}] ,
-    posts: [{id: 1 , owner: 'fabio', content: 'Meu primeiro post'}]
-};
-// CREATE
-function criaPost(dados){
-    miniRede.posts.push({
-        id: miniRede.posts.length + 1,
-        owner:dados.owner,
-        content:dados.console
-    });
-}
-criaPost({owner:'fabio' , content: 'Segundo post'})
-criaPost({owner:'fabio' , content: 'Terceiro post'})
-// READ
-function pegaPosts(){
-    return miniRede.posts;
-}
-console.log(pegaPosts())
-// UPDATE
-function atualizaContentPost(id, novoConteudo){
-    const postQueVaiSerAtualizado = pegaPosts().find((post) =>{
-        return post.id === id;
-    })
-    console.log(postQueVaiSerAtualizado)
-    postQueVaiSerAtualizado.content = novoConteudo
-}
-atualizaContentPost(1,'Novo Post')
-console.log(pegaPosts())
+// // [CRUD BÁSICO]
+// const miniRede = {
+//     usuarios:[{username: 'fabio'}] ,
+//     posts: [{id: 1 , owner: 'fabio', content: 'Meu primeiro post'}]
+// };
+// // CREATE
+// function criaPost(dados){
+//     miniRede.posts.push({
+//         id: miniRede.posts.length + 1,
+//         owner:dados.owner,
+//         content:dados.console
+//     });
+// }
+// criaPost({owner:'fabio' , content: 'Segundo post'})
+// criaPost({owner:'fabio' , content: 'Terceiro post'})
+// // READ
+// function pegaPosts(){
+//     return miniRede.posts;
+// }
+// console.log(pegaPosts())
+// // UPDATE
+// function atualizaContentPost(id, novoConteudo){
+//     const postQueVaiSerAtualizado = pegaPosts().find((post) =>{
+//         return post.id === id;
+//     })
+//     console.log(postQueVaiSerAtualizado)
+//     postQueVaiSerAtualizado.content = novoConteudo
+// }
+// atualizaContentPost(1,'Novo Post')
+// console.log(pegaPosts())
 
-// DELETE
-function apagaPost(id){
-    const listaDePostAtualizada = pegaPosts().filter((postAtual) =>{
-        return postAtual.id !== id;
-    })
-    miniRede.posts = listaDePostAtualizada;
-}
-apagaPost(2);
-console.log(pegaPosts());
+// // DELETE
+// function apagaPost(id){
+//     const listaDePostAtualizada = pegaPosts().filter((postAtual) =>{
+//         return postAtual.id !== id;
+//     })
+//     miniRede.posts = listaDePostAtualizada;
+// }
+// apagaPost(2);
+// console.log(pegaPosts());
