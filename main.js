8ddac2f76c0bfdc6f122405a16ddc71619fbb51d
Fábio Teixeira <fabio.teixeira18@hotmail.com>
2025-01-22 17:09:48 +0000

    Dark/Light Mode Update

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function enviarMensagem(){ //Função para enviar a mensagem, por enquanto vai a
     console.log(mensagem);
 }
 
-// [ARMAZENAR VIRIÁVEL NO HTML ]
+// [ARMAZENAR VIRIÁVEL NO HTML  ]
 
 document.addEventListener('DOMContentLoaded', () => {
-    let contador = 0; // Inicializa um contador para gerar IDs únicos para as tarefas
+    let contador = 0; //  Inicializa um contador para gerar IDs únicos para as tarefas
 
     // Define uma ação ao clicar no botão "Adicionar Tarefa" 
     document.getElementById('button').onclick = function() {
